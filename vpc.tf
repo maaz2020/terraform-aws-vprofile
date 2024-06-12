367eeb47ecf2e4272f13931d3144ac3dfa230494
maaz2020 <maaz.amjad@gmail.com>
2024-06-12 12:51:02 +0530

    security group added

--- vpc.tf
+++ vpc.tf
@@ -1,18 +1,18 @@
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = var.VPC_NAME
-  cidr = var.VpcCIDR
-  azs                 = [var.Zone1, var.Zone2, var.Zone3]
-  private_subnets     = [var.PrvSub1CIDR, var.PrvSub2CIDR, var.PrvSub3CIDR]
-  public_subnets      = [var.PubSub1CIDR, var.PubSub2CIDR, var.PubSub3CIDR]
-  enable_nat_gateway = true
-  single_nat_gateway = true
+  name                 = var.VPC_NAME
+  cidr                 = var.VpcCIDR
+  azs                  = [var.Zone1, var.Zone2, var.Zone3]
+  private_subnets      = [var.PrvSub1CIDR, var.PrvSub2CIDR, var.PrvSub3CIDR]
+  public_subnets       = [var.PubSub1CIDR, var.PubSub2CIDR, var.PubSub3CIDR]
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
   enable_dns_hostnames = true
   enable_dns_support   = true
 
   tags = {
-    Terraform = "true"
+    Terraform   = "true"
     Environment = "Prod"
   }
 
