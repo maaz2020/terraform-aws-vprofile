95f74d358b6536d9d9b0e098fc3f386fab099ef6
maaz2020 <maaz.amjad@gmail.com>
2024-06-21 18:30:59 +0530

    bean env updated file

--- bean-app.tf
+++ bean-app.tf
@@ -1,3 +1,3 @@
-resource "aws_elastic_beanstalk_application" "vprofile-prod-maaz" {
-  name = "vprofile-prod-maaz"
+resource "aws_elastic_beanstalk_application" "vprofile-prod" {
+  name = "vprofile-prod"
 }
