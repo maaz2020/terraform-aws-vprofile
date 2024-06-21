63d666e15e2d50a04c592c4a6878723cf348b91d
maaz2020 <maaz.amjad@gmail.com>
2024-06-21 15:26:20 +0530

    bean env updated 3

--- bean-env.tf
+++ bean-env.tf
@@ -1,7 +1,7 @@
 resource "aws_elastic_beanstalk_environment" "vprofile-bean-prod" {
   application         = aws_elastic_beanstalk_application.vprofile-prod.name
   name                = "vprofile-bean-prod"
-  solution_stack_name = "64bit Amazon Linux 2 v4.5.2 running Tomcat 8.5 Corretto 11"
+  solution_stack_name = "64bit Amazon Linux 2 v4.5.3 running Tomcat 8.5 Corretto 11"
   cname_prefix        = "vprofile-bean-prod-domain"
   setting {
     name      = "VPCId"
