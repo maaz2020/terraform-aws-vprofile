3058b9850196e8b9b836e44a7f979cd615ca23b2
maaz2020 <maaz.amjad@gmail.com>
2024-07-15 23:53:35 +0530

    bastion host file updated again

--- bastion-host.tf
+++ bastion-host.tf
@@ -1,18 +1,19 @@
 resource "aws_instance" "vprofile-bastion" {
-  ami           = lookup(var.AMIS, var.AWS_REGION )
-  instance_type = "t2.micro"
-  key_name = aws_key_pair.vprofilekey.key_name
-  subnet_id = module.vpc.public_subnets[0]
-  count = var.instance_count
+  ami                    = lookup(var.AMIS, var.AWS_REGION)
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.vprofilekey.key_name
+  subnet_id              = module.vpc.public_subnets[0]
+  count                  = var.instance_count
   vpc_security_group_ids = [aws_security_group.vprofile-bastion-sg.id]
+  associate_public_ip_address = true
 
   tags = {
-    name = "vprofile-bastion"
+    name    = "vprofile-bastion"
     PROJECT = "vprofile"
   }
 
   provisioner "file" {
-    content = templatefile("templates/db-deploy.tmpl", {rds-endpoint = aws_db_instance.vprofile-rds.address, dbuser = var.dbuser, dbpass = var.dbpass} )
+    content     = templatefile("templates/db-deploy.tmpl", { rds-endpoint = aws_db_instance.vprofile-rds.address, dbuser = var.dbuser, dbpass = var.dbpass })
     destination = "/tmp/vprofile-dbdeploy.sh"
   }
   provisioner "remote-exec" {
@@ -22,9 +23,9 @@ resource "aws_instance" "vprofile-bastion" {
     ]
   }
   connection {
-    user = var.username
+    user        = var.username
     private_key = file(var.PRIV_KEY_PATH)
-    host = self.public_ip
+    host        = self.public_ip
   }
   depends_on = [aws_db_instance.vprofile-rds]
 }
